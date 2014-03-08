0c8494ff8a6b55ccac7bedd80d2d714f5a9334a9
Kevin Griffin <xgriffin.kevin@gmail.com>
2014-03-08 20:49:39 +0900

    Add v2 of spec for webmock

---
+++ vendor/versions/v2/payment_processor_spec.rb
@@ -0,0 +1,111 @@
+require 'spec_helper'
+
+describe PaymentProcessor do
+  describe '.process' do
+
+    subject(:processor) { PaymentProcessor.new }
+    let (:user) { User.new }
+
+    before(:each) do
+      WebMock.disable_net_connect!
+    end
+
+    context 'when succeeded' do
+
+      before(:each) do
+        mock_request(success_response)
+      end
+
+      let(:parameters) do
+        {amount: 1400, currency: 'jpy', card: @token, description: 'Successful charge'}
+      end
+
+      it 'gives the user premium access' do
+        processor.process(user, parameters)
+        expect(user.premium?).to be_true
+      end
+
+    end
+
+    context 'when failed' do
+      let(:parameters) do
+        {amount: 1400, currency: 'jpy', card: 'FAILURE_TOKEN', description: 'Failure charge'}
+      end
+
+      before(:each) do
+        mock_request(failure_response)
+      end
+
+      it 'does not give the user premium access' do
+        processor.process(user, parameters)
+        expect(user.premium?).to be_false
+      end
+
+    end
+  end
+end
+
+def mock_request(return_data)
+  stub_request(:post, 'https://sk_test_laAkglBmXuzzxpU5T3h8OUiZ:@api.stripe.com/v1/charges').to_return(body: return_data, status: 200)
+end
+
+def success_response
+  '
+{
+  "id": "ch_103cxv2aR6f6MydqHbfpBUKs",
+  "object": "charge",
+  "created": 1394275693,
+  "livemode": false,
+  "paid": true,
+  "amount": 1400,
+  "currency": "usd",
+  "refunded": false,
+  "card": {
+    "id": "card_103cxu2aR6f6MydqZ0A30M0T",
+    "object": "card",
+    "last4": "4242",
+    "type": "Visa",
+    "exp_month": 8,
+    "exp_year": 2015,
+    "fingerprint": "iMOxhFMRYQ1AHlyg",
+    "customer": null,
+    "country": "US",
+    "name": null,
+    "address_line1": null,
+    "address_line2": null,
+    "address_city": null,
+    "address_state": null,
+    "address_zip": null,
+    "address_country": null,
+    "cvc_check": null,
+    "address_line1_check": null,
+    "address_zip_check": null
+  },
+  "captured": true,
+  "refunds": [
+
+  ],
+  "balance_transaction": "txn_103cxv2aR6f6MydqewnTtauv",
+  "failure_message": null,
+  "failure_code": null,
+  "amount_refunded": 0,
+  "customer": null,
+  "invoice": null,
+  "description": "Charge for test@example.com",
+  "dispute": null,
+  "metadata": {
+  }
+}'
+
+end
+
+def failure_response
+  '
+{
+  "error": {
+    "type": "invalid_request_error",
+    "message": "You cannot use a Stripe token more than once: tok_103cy82aR6f6MydqB7tuTNB5"
+  }
+}
+  '
+end
