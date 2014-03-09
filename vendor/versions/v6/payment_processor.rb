e868045193e68de7e636f1abf74373d8295bc549
Kevin Griffin <xgriffin.kevin@gmail.com>
2014-03-09 14:19:39 +0900

    Add version with abstract result

---
+++ vendor/versions/v6/payment_processor.rb
@@ -0,0 +1,26 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
+class PaymentProcessor
+  def process(user, payment_information, connection = nil, result = nil)
+    payment_connection = connection || PaymentProcessorConnection.new
+    result             = result     || ChargeResponse.new
+
+    result_json = payment_connection.post_data(payment_information)
+
+    result.parse!(result_json)
+
+    if result.success?
+      user.paid!
+    elsif result.failure?
+      user.canceled!
+    elsif result.error?
+      user.canceled!
+    else
+      raise StandardError.new('Unknown state')
+    end
+
+    result
+  end
+end
---
+++ vendor/versions/v6/payment_processor_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+
+describe PaymentProcessor do
+  describe '.process' do
+
+    subject(:processor) { PaymentProcessor.new }
+
+    let (:user) { User.new }
+    let(:connection) { MockConnection.new }
+
+    let(:parameters) do
+      {amount: 1400, currency: 'jpy', card: 'token', description: 'Charge Test'}
+    end
+
+    context 'when succeeded' do
+
+      let(:result) { MockResult.new(status: :success) }
+
+      it 'gives the user premium access' do
+        processor.process(user, parameters, connection, result)
+        expect(user.premium?).to be_true
+        expect(connection.posted_params).to equal parameters
+      end
+
+    end
+
+    context 'when failed' do
+      let(:result) { MockResult.new(status: :failure) }
+
+      it 'does not give the user premium access' do
+        processor.process(user, parameters, connection, result)
+        expect(user.premium?).to be_false
+        expect(connection.posted_params).to equal parameters
+      end
+
+    end
+
+    context 'when errored' do
+      let(:result) { MockResult.new(status: :error) }
+
+      it 'does not give the user premium access' do
+        processor.process(user, parameters, connection, result)
+        expect(user.premium?).to be_false
+        expect(connection.posted_params).to equal parameters
+      end
+
+    end
+
+    context 'when unknown status' do
+      let(:result) { MockResult.new(status: :bad_state) }
+
+      it 'throws an exception' do
+        expect { processor.process(user, parameters, connection, result) }.to raise_error(StandardError)
+        expect(user.premium?).to be_false
+      end
+    end
+  end
+end
+
+class MockConnection
+  attr_reader :posted_params
+  def post_data(params)
+    @posted_params = params
+  end
+end
+
+class MockResult
+  def initialize(status:)
+    @status = status
+  end
+
+  def parse!(result_json)
+  end
+
+  def success?
+    @status.to_s == 'success'
+  end
+
+  def failure?
+    @status.to_s == 'failure'
+  end
+
+  def error?
+    @status.to_s == 'error'
+  end
+end
